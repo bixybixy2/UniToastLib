388aaaa801f03a007c882ee4a9c0c62678c8a614
unknown <Administrator@V-DUCUMB-SB.redmond.corp.microsoft.com>
2018-10-01 11:20:37 -0700

    Revision 13: Passing Header information into app.

--- uniToast.js
+++ uniToast.js
@@ -1,4 +1,4 @@
-function showToastNotification(title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath) {
+function showToastNotification(headerID, headerTitle, headerArguments, title, body, iconImagePath, heroImagePath, inlineImagePath) {
 // Original Code
      if (!window.Windows) return Promise.resolve(false);
 
@@ -14,9 +14,21 @@ function showToastNotification(title, body, iconImagePath, badgeImagePath, heroI
 
     // Update the background image
     var images = toastXml.getElementsByTagName('image');
+        // 'Hero' image on top of notification
     images[0].setAttribute('placement', "hero");
     images[0].setAttribute('src' , imageUrl);
+        // Inline image below notification
+    images[1].setAttribute('src' , inlineImagePath);
+        // Icon image to the left.
+    images[2].setAttribute('placement', "appLogoOverride");
+    images[2].setAttribute('hint-crop', "circle");
+    images[2].setAttribute('src', iconImagePath);
+        // Badge image 
 
+    var header = toastXml.getElementsByTagName('header');
+    header[0].setAttribute('id'= headerID)
+    header[0].setAttribute('title'= headerTitle)    
+    header[0].setAttribute('arguments'= headerArguments)
     // Set notification texts
     var textNodes = toastXml.getElementsByTagName('text');
     textNodes[0].innerText = title;
@@ -29,14 +41,18 @@ function showToastNotification(title, body, iconImagePath, badgeImagePath, heroI
  
 var toastNotificationXmlTemplate =
 `<toast>
-    <visual>
+    <header id="" title="" arguments="">
+        <visual>
         <binding template="ToastGeneric">
             <text hint-maxLines="1"></text>
             <text></text>
             <image placement="" src=""/>
+            <image src=""/>
+            <image placement="" hint-crop="" src=""/>
             <script type="text/javascript">appToastButton();</script>
         </binding>
-    </visual>
+        </visual>
+    </header>
 </toast>`;
 
 
@@ -44,11 +60,11 @@ var toastNotificationXmlTemplate =
 //
 //
 
-function uniToast(title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath) {
+function uniToast(headerID, headerTitle, headerArguments, title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath) {
     if (window.Windows) {
-showToastNotification(title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath)
+showToastNotification(headerID, headerTitle, headerArguments, title, body, iconImagePath, heroImagePath, inlineImagePath)
     } else {
-        var options = {body: body, image: heroImagePath, badge: badgeImagePath}
+        var options = {body: body, image: heroImagePath, icon: iconImagePath}
         var notification = new Notification (title, options)
     }
 }
