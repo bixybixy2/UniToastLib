76f3ab1d2ca2e124f4bbbc95b1cef9dac2dc38aa
unknown <Administrator@V-DUCUMB-SB.redmond.corp.microsoft.com>
2018-10-01 11:02:54 -0700

    Revision 12: Inline, Badge, Icon loading (But not using yet)

--- uniToast.js
+++ uniToast.js
@@ -1,43 +1,8 @@
-function showToastNotification(title, body, imagePath) {
-    
-// Test Code    
-    /*
-    var notifications = Windows.UI.Notifications;
-
-    // Get the toast notification manager for the current app.
-    var notificationManager = notifications.ToastNotificationManager;
-    
-    // The getTemplateContent method returns a Windows.Data.Xml.Dom.XmlDocument object
-    // that contains the toast notification XML content.
-    var template = notifications.toastTemplateType.toastImageAndText01;
-    var toastXml = notificationManager.getTemplateContent(notifications.ToastTemplateType[template]);
-    
-    // You can use the methods from the XML document to specify the required elements for the toast.
-    var images = toastXml.getElementsByTagName("image");
-    images[0].setAttribute("src", imageURL, "placement", imageURl);
-    
-    var textNodes = toastXml.getElementsByTagName("text");
-    textNodes.forEach(function (value, index) {
-        var textNumber = index + 1;
-        var text = "";
-        for (var j = 0; j < 10; j++) {
-            text += "Text input " + textNumber + " ";
-            //@static_cast(String)
-        }
-        value.appendChild(toastXml.createTextNode(text)); 
-    });
-    
-    // Create a toast notification from the XML, then create a ToastNotifier object
-    // to send the toast.
-    var toast = new notifications.ToastNotification(toastXml);
-    
-    notificationManager.createToastNotifier().show(toast); */
-
-
+function showToastNotification(title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath) {
 // Original Code
      if (!window.Windows) return Promise.resolve(false);
 
-    var imageUrl = /*window.location.protocol + '//' + window.location.host +*/ imagePath;
+    var imageUrl = /*window.location.protocol + '//' + window.location.host +*/ heroImagePath;
 
     // Create ToastNotification as XML Doc
     var toastXml = new Windows.Data.Xml.Dom.XmlDocument();
@@ -74,39 +39,16 @@ var toastNotificationXmlTemplate =
     </visual>
 </toast>`;
 
-var toastNotificationXmlTest =
-`<toast>
-    <visual>
-        <binding template="ToastGeneric">
-            <text hint-maxLines="1">asdf</text>
-            <text>asdf</text>
-            <image placement="hero" src="https://img00.deviantart.net/68d9/i/2017/012/d/5/polygon_fox_background_by_nikolayrr-dav49t9.png"/>
-            <script type="text/javascript">appToastButton();</script>
-        </binding>
-    </visual>
-</toast>`
-
-//var toastNotificationXmlTemplate =
-//`<toast>
-//    <visual>
-//        <binding template="ToastGeneric">
-//            <text hint-maxLines="1"></text>
-//            <text></text>
-//            <image placement="" src=""/>
-//            <script type="text/javascript">appToastButton();</script>
-//        </binding>
-//    </visual>
-//</toast>`;
 
 //
 //
 //
 
-function uniToast(title, body, imagePath) {
+function uniToast(title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath) {
     if (window.Windows) {
-showToastNotification(title, body, imagePath)
+showToastNotification(title, body, iconImagePath, badgeImagePath, heroImagePath, inlineImagePath)
     } else {
-        var options = {body: body, image: imagePath}
+        var options = {body: body, image: heroImagePath, badge: badgeImagePath}
         var notification = new Notification (title, options)
     }
 }
